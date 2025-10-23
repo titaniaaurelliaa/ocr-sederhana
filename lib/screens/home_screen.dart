4392bcf09112ba2fcd8f54c58d696c067f3a8e48
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2025-10-23 13:40:45 +0700

    "UTS: Selesai Soal 1- ListTile dan Navigasi Balik

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,17 +8,20 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Menu Utama')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (_) => const ScanScreen()),
-            );
-          },
-          child: const Text('Mulai Scan Teks'),
-        ),
-      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.camera_alt, color: Colors.blue),
+            title: const Text('Mulai Pindai Teks Baru'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const ScanScreen()),
+              );
+            },
+         )
+       ]
+      )
     );
   }
 }
--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
 class ResultScreen extends StatelessWidget {
   final String ocrText;
-
   const ResultScreen({super.key, required this.ocrText});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Hasil OCR')),
+      appBar: AppBar(
+        title: const Text('Hasil Pindai'),
+      ), // AppBar
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
-          child: SelectableText(
-            ocrText.isEmpty
-                ? 'Tidak ada teks ditemukan.'
-                : ocrText.replaceAll('\n', ' '),
-            style: const TextStyle(fontSize: 18),
-          ),
-        ),
-      ),
-    );
+          child: Text(
+            ocrText, // tampilkan teks utuh dengan baris baru
+            style: const TextStyle(fontSize: 16),
+          ), // Text
+        ), // SingleChildScrollView
+      ), // Padding
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (_) => const HomeScreen()),
+            (Route<dynamic> route) => false, // hapus semua halaman di atas HomeScreen
+          );
+        },
+        child: const Icon(Icons.home),
+      ), // FloatingActionButton
+    ); // Scaffold
   }
 }
